d33090db617cd0245c8f707de2de1662e6e45784
Erin Spencer <erin.eps.hovel@gmail.com>
2026-06-29 20:57:43 -0700

    add flow plan constants

---
+++ src/metapat/flow_plan.py
@@ -0,0 +1,5 @@
+"""Planned METAPAT flow direction."""
+
+FLOW_DIRECTION = "UCNS -> METAPAT -> EDCM"
+UCNS_SIDE_STATUS = "hmmm: planned, not implemented"
+EDCM_SIDE_STATUS = "hmmm: planned, not implemented"
