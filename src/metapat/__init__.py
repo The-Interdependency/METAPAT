5be9b690b05e1787385accb2a406793eaf8019b1
Erin Spencer <erin.eps.hovel@gmail.com>
2026-06-29 20:55:55 -0700

    add metapat package exports

---
+++ src/metapat/__init__.py
@@ -0,0 +1,24 @@
+"""METAPAT public package exports."""
+
+from .canon import (
+    ENERGY_THEORY_QUESTION,
+    PRIMITIVE_EXTENSION,
+    ROOT_SPINE,
+    TIME_DEFINITION,
+    definitions,
+    primitive_extension,
+    root_spine,
+)
+from .validation import boundary_earns_its_keep, tensor_precedes_time
+
+__all__ = [
+    "ENERGY_THEORY_QUESTION",
+    "PRIMITIVE_EXTENSION",
+    "ROOT_SPINE",
+    "TIME_DEFINITION",
+    "boundary_earns_its_keep",
+    "definitions",
+    "primitive_extension",
+    "root_spine",
+    "tensor_precedes_time",
+]
